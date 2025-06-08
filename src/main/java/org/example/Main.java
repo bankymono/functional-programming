2d59e2f30a952b27504bff809c848b9f72188a4f
Ayodeji Bankole <bankymono@gmail.com>
2025-06-08 21:03:37 +0100

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.example.Main.Gender.FEMALE;
+import static org.example.Main.Gender.MALE;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", FEMALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE)
+        );
+
+        List<Person> females = new ArrayList<>();
+
+        for(Person person : people) {
+            if(FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for(Person person : females) {
+            System.out.println(person);
+        }
+
+        people.stream().filter(person -> FEMALE.equals(person.gender))
+                .forEach(System.out::println);
+
+    }
+
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender{
+        MALE, FEMALE
+    }
+
+}
